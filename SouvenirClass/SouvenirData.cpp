58416fae1699b0bf4a6a8c7ec3262c14ab723899
Talon Baker <taalonn@gmail.com>
2015-05-10 16:01:32 -0700

    Added Souvenir Class Folder
    I've added a file containing a text file, header file and  cpp file for
    dealing with all the souvenirs within the project.
    There are methods for updating item prices, deleting items and adding
    items. All of these methods are tested and - at least for testing that I
    did - are working.
    
    There is a method updateSouvenirList() for keeping data persistent
    throughout the runs. Run that method and all the changes that have been
    made to the souvenir class will be made permanent within the text file.
    
    PLEASE NOTE: whom ever uses this class will need to change the text from
    normal C++ file implementation to the QT file implementation as I don't
    want to implement it incorrectly.
    
    I'll send out an email with the same information so we're all on the
    same page.

---
+++ SouvenirClass/SouvenirData.cpp
@@ -0,0 +1,261 @@
+/*
+ * SouvenirData.cpp
+ *
+ *  Created on: May 10, 2015
+ *      Author: TalonMachine
+ */
+
+#include "SouvenirData.h"
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <limits>
+#include <ios>
+
+//creates a souvenir class based on the text file souvenirInfoList.txt
+SouvenirData::SouvenirData() {
+	std::ifstream inFile;
+	inFile.open("souvenirInfoList.txt");
+
+	while(!inFile.eof()) {
+
+		std::vector<souvenirInfo> newSouvenirs;
+		std::string  teamName;
+		souvenirInfo newSouvenir;
+		std::string  souvenirName;
+		float        souvenirPrice;
+
+		//getting the stadium name that will hold the list of souvenirs
+		std::getline(inFile, teamName);
+
+		//getting the stadium's souvenir name
+		std::getline(inFile, souvenirName);
+
+		//taking in all the souvenirs the stadium has to offer
+		while(souvenirName != "~") {
+
+			//getting the souvenir's price
+			inFile >> souvenirPrice;
+            inFile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+            //setting the name and price to the struct
+            newSouvenir.item  = souvenirName;
+            newSouvenir.price = souvenirPrice;
+
+            //pushing the new souvenir info into the vector
+            newSouvenirs.push_back(newSouvenir);
+
+			//getting the stadium's souvenir name
+			std::getline(inFile, souvenirName);
+		}//end - while
+
+		listOfSouvenirs[teamName] =  newSouvenirs;
+	}//end - while
+
+	inFile.close();
+
+}//end - default constructor
+
+SouvenirData::~SouvenirData() { }//end - default constructor
+
+//this method will make the soucenir list presistant between runs depending on
+//the changes that were made to it.
+void SouvenirData::updateSouvenirList() {
+	std::map< std::string, std::vector<souvenirInfo> >::iterator
+									it = listOfSouvenirs.begin();
+	std::map< std::string, std::vector<souvenirInfo> >::iterator
+									endFileCheck = listOfSouvenirs.begin();
+	endFileCheck++;
+
+
+
+	//opening file for update
+    std::ofstream outFile;
+    outFile.open("souvenirInfoList.txt");
+
+    for(; it != listOfSouvenirs.end(); it++, endFileCheck++) {
+    	outFile << it->first << std::endl;
+
+        std::vector<souvenirInfo>::iterator it2 = it->second.begin();
+
+        for(; it2 != it->second.end(); it2++) {
+        	outFile << it2->item << std::endl;
+        	outFile << it2->price << std::endl;
+        }//end - for
+        outFile << "~";
+
+        if(endFileCheck != listOfSouvenirs.end()) outFile << std::endl;
+    }//end - for
+
+    outFile.close();
+
+}//end - updateSouvenirList
+
+
+//this method will print the data for all the stadiums
+std::string SouvenirData::printAll() {
+	std::map< std::string, std::vector<souvenirInfo> >::iterator
+									it = listOfSouvenirs.begin();
+	std::map< std::string, std::vector<souvenirInfo> >::iterator
+									endFileCheck = listOfSouvenirs.begin();
+	endFileCheck++;
+
+	std::stringstream returnStr;
+	returnStr << std::setprecision(2) << std::fixed;
+
+    for(; it != listOfSouvenirs.end(); it++, endFileCheck++) {
+    	returnStr << it->first << "\n";
+
+        std::vector<souvenirInfo>::iterator it2 = it->second.begin();
+
+        for(; it2 != it->second.end(); it2++) {
+        	returnStr << it2->item << "\n";
+        	returnStr << it2->price << "\n";
+        }//end - for
+        if(endFileCheck != listOfSouvenirs.end()) returnStr << "\n";
+    }//end - for
+
+    return returnStr.str();
+}//end - printAll
+
+//adding a souvenir to the passed in stadium. returns true if item was
+//added successfully
+bool SouvenirData::addSouvenir(std::string stadiumName,
+				 std::string itemName,
+				 float itemPrice) {
+	//the .count() method will return a bool if the key is in the map.
+	//if stadium is not in the map, return false.
+	if(!listOfSouvenirs.count(stadiumName)) return false;
+
+	souvenirInfo newSouvenir;
+	newSouvenir.item  = itemName;
+	newSouvenir.price = itemPrice;
+	listOfSouvenirs.at(stadiumName).push_back(newSouvenir);
+	return true;
+}//end - addSouvenir
+
+//updates the price of an item. returns true if the item's price was
+//updated successfully
+bool SouvenirData::updatePrice(std::string stadiumName,
+							   std::string itemName, float newPrice) {
+	//the .count() method will return a bool if the key is in the map.
+	//if stadium is not in the map, return false.
+	if(!listOfSouvenirs.count(stadiumName)) return false;
+
+	bool notUpdated = true;
+
+	std::vector<souvenirInfo>::iterator it = listOfSouvenirs.at(stadiumName).begin();
+	while(it != listOfSouvenirs.at(stadiumName).end() && notUpdated) {
+		//if true, the item was found and the price was updated
+		if(it->item == itemName) {
+			it->price = newPrice;
+			notUpdated = false;
+			return true;
+		}
+		it++;
+	}//end - while
+
+	//if this false is reached, this means there IS a stadium in the list, but
+	//no passed in souvenir in that sradium's shop.
+	return false;
+}//end - updatePrice
+
+//removes a souvenir from a stadium. returns true if removal was
+//successful
+bool SouvenirData::removeSouvenir(std::string stadiumName, std::string itemName) {
+	//the .count() method will return a bool if the key is in the map.
+	//if stadium is not in the map, return false.
+	if(!listOfSouvenirs.count(stadiumName)) return false;
+
+	std::vector<souvenirInfo>::iterator it = listOfSouvenirs.at(stadiumName).begin();
+	while(it != listOfSouvenirs.at(stadiumName).end()) {
+		//if true, the item was found and the price was updated
+		if(it->item == itemName) {
+			listOfSouvenirs.at(stadiumName).erase(it);
+			return true;
+		}
+		it++;
+	}//end - while
+
+	//if this false is reached, this means there IS a stadium in the list, but
+	//no passed in souvenir in that sradium's shop.
+	return false;
+}//end - removeSouvenir
+
+//returns the price of the souvenir at the stadium. returns -1 if not found
+float SouvenirData::getPrice(std::string stadiumName, std::string souvenirName) {
+	//the .count() method will return a bool if the key is in the map.
+	//if stadium is not in the map, return false.
+	if(!listOfSouvenirs.count(stadiumName)) return -1.0;
+
+	std::vector<souvenirInfo>::iterator it = listOfSouvenirs.at(stadiumName).begin();
+	while(it != listOfSouvenirs.at(stadiumName).end()) {
+		//if true, the item was found and the price was updated
+		if(it->item == souvenirName) {
+			return it->price;
+		}
+		it++;
+	}//end - while
+
+	//if this return, item was not found
+	return -1.0;
+
+}//end - getPrice
+
+//returns a string all the souvenir's information at the passed in stadium
+//returns an error message if the stadium is not found
+std::string SouvenirData::getAllInfoAt(std::string stadiumName) {
+	//the .count() method will return a bool if the key is in the map.
+	//if stadium is not in the map, return false.
+	if(!listOfSouvenirs.count(stadiumName)) return "::ERROR:: Stadium Not Found\n";
+
+	std::stringstream returnString;
+
+	returnString << stadiumName << "\n";
+
+	std::vector<souvenirInfo>::iterator it = listOfSouvenirs.at(stadiumName).begin();
+	for(;it != listOfSouvenirs.at(stadiumName).end(); it++) {
+		returnString << it->item << "\n";
+		returnString << it->price << "\n";
+	}//end - while
+
+	//if this return, item was not found
+	return returnString.str();
+}//end - getAllInfoAt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
